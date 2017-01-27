dd93e6f41612e0b9592c730dbbefbe543b870ec8
Vince Cellini <vincecellini@Vinces-MacBook-Pro.local>
2017-01-26 20:17:46 -0500

    complete

--- ex-07-bonus-makePairs.js
+++ ex-07-bonus-makePairs.js
@@ -1,19 +1,38 @@
 /** ! Adventure Mode !
  * Ex : makePairs()
  *
- * Write a function called makePairs that takes an array of values and 
+ * Write a function called makePairs that takes an array of values and
  * returns an an array of pair-arrays.
  *
  * e.g. makePairs(['hi','hello', 'howdy', 'greetings'])
  *       => [ ['hi', 'hello'] , ['howdy', 'greetings'] ]
  *
 */
- 
+ var makePairs = function(someArray){
+   var superArray = []
+   var smallArray = []
+   for(var i = 0; i < someArray.length; i++){
+     console.log(someArray[i])
+     if(smallArray.length < 2){
+       smallArray.push(someArray[i]);
+     }
+     if(smallArray.length === 2){
+         superArray.push(smallArray);
+         console.log('the pair', smallArray);
+         console.log('the output', superArray);
+         smallArray = []
+     }
+    }
+
+    return superArray
+   }
 
 
 
 
-var pairsOutput = makePairs([10,20,30,40,50,60,70,80]) 
+
+
+var pairsOutput = makePairs([10,20,30,40,50,60,70,80])
 //=> [ [10,20], [30,40], [50,60], [70,80] ]
 
 var firstPair = pairsOutput[0];
